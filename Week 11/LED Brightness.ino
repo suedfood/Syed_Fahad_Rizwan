df7ff45bc6f983ced92b5c1b5c0d139c82418e74
Syed Fahad Rizwan <98391108+suedfood@users.noreply.github.com>
2022-04-17 21:12:01 +0400

    Create LED Brightness.ino

---
+++ Week 11/LED Brightness.ino
@@ -0,0 +1,20 @@
+const int ledPin = 3; // led is connected to pin 3
+
+void setup() {
+  // initializes ledPin as an output
+  pinMode(ledPin,OUTPUT);
+  // initializes serial communication at baud rate
+  Serial.begin(9600);
+}
+
+void loop() {
+  // variable to control brightness
+  int ledBrightness;
+  // checks if information received from p5js
+  if (Serial.available() > 0) {
+    // stores the most recent byte between 0-255
+    ledBrightness = Serial.read();
+    // changes the brightness of led depending on data received
+    analogWrite(ledPin,ledBrightness);
+  }
+}
